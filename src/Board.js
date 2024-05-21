bba3d53a1725eb7c2515ce48f20e6d1fc9031e1f
jett-sloan <jettsloansloan@gmail.com>
2024-05-21 14:08:39 -0400

    Update something

--- src/Board.js
+++ src/Board.js
@@ -1,113 +1,75 @@
 import React, { useState } from "react";
 import Cell from "./Cell";
-import "./Board.css";
-//import { getTreeDiff } from "@testing-library/user-event/dist/cjs/utils/index.js";
+//import "./Board.css";
 
-/** Game board of Lights out.
- *
- * Properties:
- *
- * - nrows: number of rows of board
- * - ncols: number of cols of board
- * - chanceLightStartsOn: float, chance any cell is lit at start of game
- *
- * State:
- *
- * - board: array-of-arrays of true/false
- *
- *    For this board:
- *       .  .  .
- *       O  O  .     (where . is off, and O is on)
- *       .  .  .
- *
- *    This would be: [[f, f, f], [t, t, f], [f, f, f]]
- *
- *  This should render an HTML table of individual <Cell /> components.
- *
- *  This doesn't handle any clicks --- clicks are on individual cells
- *
- **/
-
-function Board({ nrows, ncols, chanceLightStartsOn }) {
+function Board({ nrows = 5, ncols = 5, chanceLightStartsOn = 0.25 }) {
   const [board, setBoard] = useState(createBoard());
 
-  /** create a board nrows high/ncols wide, each cell randomly lit or unlit */
   function createBoard() {
     let initialBoard = [];
     for (let i = 0; i < nrows; i++) {
-      let row = []
-
+      let row = [];
       for (let j = 0; j < ncols; j++) {
-        row.push(Math.random() < chanceLightStartsOn)
-
+        row.push(Math.random() < chanceLightStartsOn);
       }
-      initialBoard.push.row()
+      initialBoard.push(row);
     }
-
-
-    // TODO: create array-of-arrays of true/false values
     return initialBoard;
   }
 
   function hasWon() {
-    // TODO: check the board in state to determine whether the player has won.
-    if (board.every(row => row.every(cell => !cell)))
-      return true
-    else
-      return false
+    return board.every(row => row.every(cell => !cell));
   }
 
   function flipCellsAround(coord) {
     setBoard(oldBoard => {
+      console.log(coord)
       const [y, x] = coord.split("-").map(Number);
-
-      const flipCell = (y, x, boardCopy) => {
-        // if this coord is actually on board, flip it
-
+      const boardCopy = oldBoard.map(row => [...row]);
+      const flipCell = (y, x) => {
+        console.log(x,y,boardCopy)
         if (x >= 0 && x < ncols && y >= 0 && y < nrows) {
           boardCopy[y][x] = !boardCopy[y][x];
         }
       };
-
-      // TODO: Make a (deep) copy of the oldBoard
-      const boardCopy = oldBoard.map(row => [...row]);
-
-      // TODO: in the copy, flip this cell and the cells around it
-      flipCell(y, x, boardCopy)
-      flipCell(y, x - 1, boardCopy)
-      flipCell(y, x + 1, boardCopy)
-      flipCell(y - 1, x, boardCopy)
-      flipCell(y + 2, x, boardCopy)
-      // TODO: return the copy
-      return boardCopy
+      flipCell(y, x);
+      flipCell(y, x - 1);
+      flipCell(y, x + 1);
+      flipCell(y - 1, x);
+      flipCell(y + 1, x);
+      return boardCopy;
     });
   }
 
-  // if the game is won, just show a winning msg & render nothing else
-  //if (hasWon()) {
-  //  alert('You Won')
-  //}
-  // TODO
+  if (hasWon()) {
+    alert("You Won");
+  }
 
-  // make table board
   const makeTable = () => {
     return (
       <table className="game">
         <tbody>
-          {board.map((row, y) =>
+          {board.map((row, y) => (
             <tr key={y}>
-              {row.map((cell, x) =>
-                <td>{Cell(flipCellsAround,cell)}</td>
-              )}
+              {row.map((cell, x) => (
+                <td key={`${y}-${x}`}>
+                  <Cell
+                    key={`${y}-${x}`}
+                    flipCellsAroundMe={() => flipCellsAround(`${y}-${x}`)} // Ensure the function is properly bound
+                    isLit={cell}
+                    coord={`${y}-${x}`}
+                  />
+                </td>
+              ))}
             </tr>
-          )}
+          ))}
         </tbody>
       </table>
     );
   };
+  
 
-  return (<div>{makeTable()}</div>)
-  // TODO
+  return <div>{makeTable()}</div>;
 }
 
 export default Board;
